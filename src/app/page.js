918eff536fab485033adc494525f7d9478c76799
Eduardo Calachua <jocdwardo25@gmail.com>
2024-01-02 17:00:39 -0500

    add: hook useTask to make map, TaskCard to render tasks and useRouter to redirect to a new view

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,25 @@
+"use client"
+import { useTask } from '@/context/TasksContext'
+import { TaskCard } from '@/components/TaskCard'
+import { useRouter } from 'next/navigation'
+
 const page = () => {
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const {tasks} = useTask()
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const router = useRouter()
   return (
-    <div>Home</div>
+    <div className='max-w-xl mx-auto'>
+      {tasks.map(({id, title, description}, index)=>(
+        <TaskCard key={index} id={id} title={title} description={description}/>
+      ))}
+      <button 
+        onClick={()=> router.push('/new')}
+        className='bg-green-600 py-3 rounded-lg my-8 w-full'
+      >
+        ADD TASK
+      </button>
+    </div>
   )
 }
 
