342a89e0f7e5a27edf9757080efae720ab2214d8
Eduardo Calachua <jocdwardo25@gmail.com>
2024-01-02 16:11:40 -0500

    add: useRouter and button

--- src/app/edit/[id]/page.js
+++ src/app/edit/[id]/page.js
@@ -1,8 +1,19 @@
 "use client"
+import { useRouter } from 'next/navigation'
 
 const page = ({params}) => {
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const router = useRouter()
   return (
-    <div>ID:{params.id}</div>
+    <div>
+      <h1>ID:{params.id}</h1>
+      <button 
+        className='bg-blue-600 py-2 px-3 rounded-lg mt-4'
+        onClick={()=> router.push('/')}
+      >
+        Return
+      </button>
+    </div>
   )
 }
 
