8e412e2e926bc26b0fcaed38778abf5dc3815ff0
Eduardo Calachua <jocdwardo25@gmail.com>
2024-01-02 17:06:09 -0500

    add : form, useRouter to redirect to home, useTask to create new tasks and useState to catch name and value

--- src/app/new/page.js
+++ src/app/new/page.js
@@ -1,6 +1,63 @@
+"use client"
+import { useRouter } from 'next/navigation'
+import { useState } from 'react'
+import { useTask } from '@/context/TasksContext'
+
 const page = () => {
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const router = useRouter()
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const [task, setTask] = useState()
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const { createTask } = useTask()
+  const handleChange = (e) => setTask({...task,[e.target.name]: e.target.value})
+  const handleSubmit = (e) =>{
+    e.preventDefault()
+    console.log(task)
+    createTask(task.title, task.description)
+    router.push('/')
+  }
   return (
-    <div>New</div>
+    <div className='h-screen flex items-center'>
+      <form 
+        className='flex flex-col  max-w-sm mx-auto bg-gray-700 rounded-lg py-10 px-5 text-black'
+        onSubmit={handleSubmit}
+        >
+        <input
+          name='title'
+          placeholder='Escribe el Titulo'
+          className='text-3xl font-semibold rounded-lg'
+          onChange={handleChange}
+        />
+        <textarea 
+          name='description'
+          placeholder='Escribe la descripcion'
+          className='mt-4 rounded-lg'
+          onChange={handleChange}
+          />
+        <div className='flex gap-8'>
+          <button 
+            className='bg-green-600 py-3 rounded-lg mt-8 w-full text-white'
+            >
+              Save
+          </button>
+          <button
+            className='bg-red-600 py-3 rounded-lg mt-8 w-full text-white'
+            type='button'
+            onClick={()=> router.push('/')}
+            >
+            Cancel
+          </button>
+        </div>
+        <button
+          className='bg-blue-600 py-3 rounded-lg mt-8 w-full text-white'
+          type='button'
+          onClick={()=> router.push('/')}
+        >
+          Return
+        </button>
+      </form>
+    </div>
   )
 }
 
