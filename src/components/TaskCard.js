d590ff2a175e984a3c38611eab115422aebc7bb8
Eduardo Calachua <jocdwardo25@gmail.com>
2024-01-02 22:48:23 -0500

    add: button edit

--- src/components/TaskCard.js
+++ src/components/TaskCard.js
@@ -5,12 +5,20 @@ export const TaskCard = ({id, title, description}) => {
   return (
     <div 
       className='m-10 bg-gray-700 rounded-lg max-w-xl mx-auto p-5'
-      onClick={() => router.push(`/edit/${id}`)}
+      // onClick={() => router.push(`/edit/${id}`)}
     >
         <h1 className='text-3xl font-semibold'>{title}</h1>
-        <button className='bg-red-600 py-2 px-3 rounded-lg mt-4'>
-          Delete
-        </button>
+        <div className=' flex gap-5'>
+          <button className='bg-red-600 py-2 px-3 rounded-lg mt-4'>
+            Delete
+          </button>
+          <button 
+            className='bg-blue-600 py-2 px-6 rounded-lg mt-4'
+            onClick={()=> router.push(`/edit/${id}`)}
+          >
+            Edit
+          </button>
+        </div>
         <p className='mt-4'>{description}</p>
     </div>
   )
