524d4547f0807e3d4d0495cb0b5c97cb0cb0be48
Eduardo Calachua <jocdwardo25@gmail.com>
2024-01-03 03:32:20 -0500

    add: deleteTask to button

--- src/components/TaskCard.js
+++ src/components/TaskCard.js
@@ -1,7 +1,9 @@
 import { useRouter } from 'next/navigation'
+import { useTask } from '@/context/TasksContext'
 
 export const TaskCard = ({id, title, description}) => {
   const router = useRouter()
+  const {deleteTask} = useTask()
   return (
     <div 
       className='m-10 bg-gray-700 rounded-lg max-w-xl mx-auto p-5'
@@ -9,7 +11,13 @@ export const TaskCard = ({id, title, description}) => {
     >
         <h1 className='text-3xl font-semibold'>{title}</h1>
         <div className=' flex gap-5'>
-          <button className='bg-red-600 py-2 px-3 rounded-lg mt-4'>
+          <button 
+            className='bg-red-600 py-2 px-3 rounded-lg mt-4'
+            onClick={(e) => {
+              e.stopPropagation()
+              deleteTask(id)
+            }}
+          >
             Delete
           </button>
           <button 
